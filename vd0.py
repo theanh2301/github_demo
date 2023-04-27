c764fdcd4f6a52c9cd2f20b69093ccbcd6619ef5
theanh2301 <becam0582@gmail.com>
2023-04-27 08:29:38 +0700

    add vd00.py file

---
+++ vd0.py
@@ -0,0 +1,14 @@
+# this is a comment.
+print("Hello!")
+# correct syntax
+if 5 > 2:
+    print("5 is greater than 2")
+    print("2 is greater than 5")
+# wrong syntax
+'''
+if(5>2):
+    print('5 is greater than 2')
+        print('5 is greater than 2')'''
+'''
+if (5 > 5):
+print('5 is greater than 2')'''
